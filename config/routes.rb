ef0b4a38cd6a6dd0f6351ed2afe53c8c4cc8a19e
Violette Dunoyer de S <violette.dunoyer@gmail.com>
2022-05-16 12:12:23 +0200

    stupid coach

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get 'ask', to: 'questions#ask'
+end
+
+Rails.application.routes.draw do
+  get 'answer', to: 'questions#answer'
 end
